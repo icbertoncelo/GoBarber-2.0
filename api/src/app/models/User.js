f0d6fc7ab685a9c8331fce35f95c56d6a4674736
Ian Carlos <carlos.ian007@gmail.com>
2019-07-17 15:32:31 -0300

    Feat(api): Add user/session controller and user model

---
+++ api/src/app/models/User.js
@@ -0,0 +1,39 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import authConfig from '../../config/auth';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+
+  generateToken({ id }) {
+    return jwt.sign({ id }, authConfig.secret, { expiresIn: authConfig.ttl });
+  }
+}
+
+export default User;
