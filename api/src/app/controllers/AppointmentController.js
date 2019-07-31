ebce87c38abdb841a1d3401717a63abaacad9920
Ian Carlos <carlos.ian007@gmail.com>
2019-07-31 15:14:36 -0300

    Feat(api): Add past and cancelable fields in appointments model

--- api/src/app/controllers/AppointmentController.js
+++ api/src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
 
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       order: ['date'],
       limit: 20,
       offset: (page - 1) * 20,
