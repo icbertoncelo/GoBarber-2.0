19787da38a629ae3283413b38b980edb8e620145
Ian Carlos <carlos.ian007@gmail.com>
2019-07-31 15:55:31 -0300

    Feat(api): Add Sentry and Youch for Exception Handling

--- api/src/app.js
+++ api/src/app.js
@@ -2,18 +2,28 @@ import 'dotenv/config';
 
 import express from 'express';
 import { resolve } from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
+import sentryconfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryconfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -23,6 +33,19 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal error server' });
+    });
   }
 }
 
