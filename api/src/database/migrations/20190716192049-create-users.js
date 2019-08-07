dae9867fa21099d352255624d1365a0c4b905e48
Ian Carlos <carlos.ian007@gmail.com>
2019-08-07 15:15:07 -0300

    Feat(api): Add a new migration to insert the avatar_id field in the user model

--- api/src/database/migrations/20190716192049-create-users.js
+++ api/src/database/migrations/20190716192049-create-users.js
@@ -25,13 +25,6 @@ module.exports = {
         defaultValue: false,
         allowNull: false,
       },
-      avatar_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'users', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
