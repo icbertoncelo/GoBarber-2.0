d863dedbc945352f703accb5e7e76de617d63920
Ian Carlos <carlos.ian007@gmail.com>
2019-08-16 09:46:28 -0300

    Style(auth): Add signIn/signUp pages

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
 
 export default function SignIn() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Access</button>
+        <Link to="/register">Create new account</Link>
+      </Form>
+    </>
+  );
 }
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must have at least 6 characters')
+    .required('Password is required'),
+});
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Name" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Create account</button>
+        <Link to="/">Go back to login</Link>
+      </Form>
+    </>
+  );
 }
