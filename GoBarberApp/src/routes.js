0ec97ecf8055f0ad52cc209ec51d426e60f0637f
Ian Carlos <carlos.ian007@gmail.com>
2019-08-26 14:30:21 -0300

    Feat(mobile): Add scheduling route

--- GoBarberApp/src/routes.js
+++ GoBarberApp/src/routes.js
@@ -1,11 +1,20 @@
+import React from 'react';
+
 import {
   createAppContainer,
   createSwitchNavigator,
   createBottomTabNavigator,
+  createStackNavigator,
 } from 'react-navigation';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/auth/SignIn';
 import SignUp from './pages/auth/SignUp';
+
+import SelectProvider from './pages/new/SelectProvider';
+import SelectDateTime from './pages/new/SelectDateTime';
+import Confirm from './pages/new/Confirm';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -21,6 +30,38 @@ export default (isSigned = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTitleStyle: {
+                      fontWeight: 'bold',
+                    },
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Schedule',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
